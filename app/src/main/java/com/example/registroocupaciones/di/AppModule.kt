cc75daad14273df7f478aeb397fa6903cec02cd6
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-05-10 10:15:44 -0400

    crea el AppModule

---
+++ app/src/main/java/com/example/registroocupaciones/di/AppModule.kt
@@ -0,0 +1,42 @@
+package com.example.registroocupaciones.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.registroocupaciones.data.database.OcupacionDb
+import com.example.registroocupaciones.data.ocupaciones.local.OcupacionDao
+import com.example.registroocupaciones.data.ocupaciones.repository.OcupacionRepositoryImpl
+import com.example.registroocupaciones.domain.ocupaciones.repository.OcupacionRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOcupacionDb(@ApplicationContext appContext: Context): OcupacionDb.OcupacionDB {
+        return Room.databaseBuilder(
+            appContext,
+            OcupacionDb.OcupacionDB::class.java,
+            "Ocupacion.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOcupacionDao(ocupacionDb: OcupacionDb.OcupacionDB): OcupacionDao {
+        return ocupacionDb.ocupacionesDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOcupacionRepository(ocupacionDao: OcupacionDao): OcupacionRepository {
+        return OcupacionRepositoryImpl(ocupacionDao)
+    }
+}
