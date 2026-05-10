b126ef6a090c467b46d91a358d75ff53ba2700ad
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-05-10 10:30:05 -0400

    crea el RegistroOcupacionApp

---
+++ app/src/main/java/com/example/registroocupaciones/RegistroOcupacionApp.kt
@@ -0,0 +1,8 @@
+package com.example.registroocupaciones
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroOcupacionApp : Application() {
+}
