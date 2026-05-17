b063c650245921fc610687136435bd53a07c34f8
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-05-17 12:55:45 -0400

    clases removidas y archivos vacios removidos

--- app/src/main/java/com/example/registroocupaciones/domain/ocupaciones/model/ocupacion.kt
+++
@@ -1,7 +0,0 @@
-package com.example.registroocupaciones.domain.ocupaciones.model
-
-data class Ocupacion(
-    val ocupacionId: Int? = null,
-    val descripcion: String = "",
-    val sueldo: Double = 0.0
-)
--- app/src/main/java/com/example/registroocupaciones/domain/ocupaciones/usecase/DeteleOcupacionUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.registroocupaciones.domain.ocupaciones.usecase
-
-import com.example.registroocupaciones.domain.ocupaciones.model.Ocupacion
-import com.example.registroocupaciones.domain.ocupaciones.repository.OcupacionRepository
-import jakarta.inject.Inject
-
-class DeleteOcupacionUseCase @Inject constructor(private val repo: OcupacionRepository) {
-    suspend operator fun invoke(ocupacion: Ocupacion) = repo.delete(ocupacion)
-}
