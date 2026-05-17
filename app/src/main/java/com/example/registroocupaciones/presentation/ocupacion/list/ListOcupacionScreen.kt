d1984080b5ce6e466091086246a743cf2043e875
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-05-17 12:58:12 -0400

    moviendo clase a carpeta correspondiente

--- app/src/main/java/com/example/registroocupaciones/presentation/ocupacion/list/ListOcupacionScreen.kt
+++ app/src/main/java/com/example/registroocupaciones/presentation/ocupacion/list/ListOcupacionScreen.kt
@@ -1,4 +1,4 @@
-package com.example.registroocupaciones.presentation.list
+package com.example.registroocupaciones.presentation.ocupacion.list
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
