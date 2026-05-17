5014d5ac8c98bcb0ec4ce0011042d778725205b8
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-05-17 13:07:24 -0400

    modifica el RegistroNavHost.kt y agrega funciones de empleados

--- app/src/main/java/com/example/registroocupaciones/presentation/navegation/RegistroNavHost.kt
+++ app/src/main/java/com/example/registroocupaciones/presentation/navegation/RegistroNavHost.kt
@@ -1,25 +1,35 @@
 package com.example.registroocupaciones.presentation.navigation
 
+import androidx.compose.material3.DrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import com.example.registroocupaciones.presentation.list.OcupacionListScreen
-import com.example.registroocupaciones.presentation.edit.EditOcupacionScreen
+import com.example.registroocupaciones.presentation.ocupacion.list.OcupacionListScreen
+import com.example.registroocupaciones.presentation.ocupacion.edit.EditOcupacionScreen
+import com.example.registroocupaciones.presentation.empleado.list.EmpleadoListScreen
+import com.example.registroocupaciones.presentation.empleado.edit.EditEmpleadoScreen
 import com.example.registroocupaciones.presentation.navegation.Screen
+import kotlinx.coroutines.launch
 
 @Composable
 fun RegistroNavHost(
-    navHostController: NavHostController
+    navHostController: NavHostController,
+    drawerState: DrawerState
 ) {
+    val scope = rememberCoroutineScope()
+
     NavHost(
         navController = navHostController,
         startDestination = Screen.OcupacionList
     ) {
         composable<Screen.OcupacionList> {
             OcupacionListScreen(
-                onDrawer = { },
+                onDrawer = {
+                    scope.launch { drawerState.open() }
+                },
                 goToOcupacion = { id ->
                     navHostController.navigate(Screen.Ocupacion(id))
                 },
@@ -29,13 +39,39 @@ fun RegistroNavHost(
             )
         }
 
-
         composable<Screen.Ocupacion> {
             val args = it.toRoute<Screen.Ocupacion>()
             EditOcupacionScreen(
                 ocupacionId = args.ocupacionId,
                 onNavigateBack = { navHostController.navigateUp() },
-                onDrawer = {}
+                onDrawer = {
+                    scope.launch { drawerState.open() }
+                }
+            )
+        }
+
+        composable<Screen.EmpleadoList> {
+            EmpleadoListScreen(
+                onDrawer = {
+                    scope.launch { drawerState.open() }
+                },
+                goToEmpleado = { id ->
+                    navHostController.navigate(Screen.Empleado(id))
+                },
+                createEmpleado = {
+                    navHostController.navigate(Screen.Empleado(0))
+                }
+            )
+        }
+
+        composable<Screen.Empleado> {
+            val args = it.toRoute<Screen.Empleado>()
+            EditEmpleadoScreen(
+                empleadoId = args.empleadoId,
+                onNavigateBack = { navHostController.navigateUp() },
+                onDrawer = {
+                    scope.launch { drawerState.open() }
+                }
             )
         }
     }
